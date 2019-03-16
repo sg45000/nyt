1994da3d6fe311eed697311dc1d412419c031b43
sg45000 <hamakyo711@gmail.com>
2019-03-16 15:15:15 +0900

    refactor OK

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 Vue.config.devtools = true;
+
+const NYBaseUrl = "https://api.nytimes.com/svc/topstories/v2/"
+const ApiKey = "MsnZYe16x2y84tNTgwY47KTyhFPfQdR6";
+
+function buildUrl (url){
+  return NYBaseUrl + url + ".json?api-key=" + ApiKey
+}
 const vm = new Vue({
   el: '#app',
   data: {
     results: []
   },
   mounted() {
-    axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=MsnZYe16x2y84tNTgwY47KTyhFPfQdR6")
-    .then(response => {this.results = response.data.results})
+    this.getPosts('home');
+  },
+  methods:{
+    getPosts(section){
+      let url = buildUrl(section);
+      axios.get(url).then(response => {
+        this.results = response.data.results
+      }).catch(error => {console.log(error);});
+    }
+  },
+  computed: {
+    processedPosts(){
+      let posts = this.results;
+
+      //Add image_url attribute
+      posts.map(post => {
+        let imgObj = post.multimedia.find(media => media.format === "superJumbo");
+        post.image_url =imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
+      });
+
+      //Put Array into Chunks
+      let i, j, chunkedArray = [], chunk = 4;
+      for (i=0, j=0; i < posts.length; i += chunk, j++){
+        chunkedArray[j] = posts.slice(i, i+chunk);
+      }
+      return chunkedArray;
+    }
   }
-})
+});
