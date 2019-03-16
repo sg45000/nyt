bdc57817a13e2751546c71853f6ca955538bc249
Kyo Hamabe <hamakyo711@gmail.com>
2019-03-16 13:32:54 +0900

    create demo

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const vm = new Vue({
+  el: '#app',
+  data: {
+    results: [
+      {title: "the very first post", abstract: "lorem ipsum some test dimpsum"},
+      {title: "and then there was the second", abstract: "lorem ipsum some test dimpsum"},
+      {title: "third time's a charm", abstract: "lorem ipsum some test dimpsum"},
+      {title: "four the last time", abstract: "lorem ipsum some test dimpsum"}
+    ]
+  }
+})
