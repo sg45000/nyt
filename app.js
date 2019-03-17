32d041becf3f5ee6fd5130f344143a3858518ab1
sg45000 <hamakyo711@gmail.com>
2019-03-17 17:10:22 +0900

    Replace component

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
 Vue.config.devtools = true;
 
-const NYBaseUrl = "https://api.nytimes.com/svc/topstories/v2/"
+Vue.component('news-list',{
+  props: ['results'],
+  template: `
+  <section>
+  <div class="row" v-for="posts in processedPosts">
+    <div class="columns large-3 medium-6" v-for="post in posts">
+      <div class="card">
+        <div class="card-divider">
+          {{post.title}}
+        </div>
+        <a :href="post.url" target="_blank"><img :src="post.image_url"></a>
+        <div class="card-section">
+          <p>{{post.abstract}}.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+  `,
+computed: {
+  processedPosts(){
+    let posts = this.results;
+
+    //Add image_url attribute
+    posts.map(post => {
+      let imgObj = post.multimedia.find(media => media.format === "superJumbo");
+      post.image_url =imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
+    });
+
+    //Put Array into Chunks
+    let i, j, chunkedArray = [], chunk = 4;
+    for (i=0, j=0; i < posts.length; i += chunk, j++){
+      chunkedArray[j] = posts.slice(i, i+chunk);
+    }
+    return chunkedArray;
+  }
+}
+});
+
+const NYBaseUrl = "https://api.nytimes.com/svc/topstories/v2/";
 const ApiKey = "MsnZYe16x2y84tNTgwY47KTyhFPfQdR6";
 
 function buildUrl (url){
   return NYBaseUrl + url + ".json?api-key=" + ApiKey
 }
+
 const vm = new Vue({
   el: '#app',
   data: {
@@ -21,23 +61,6 @@ const vm = new Vue({
         this.results = response.data.results
       }).catch(error => {console.log(error);});
     }
-  },
-  computed: {
-    processedPosts(){
-      let posts = this.results;
-
-      //Add image_url attribute
-      posts.map(post => {
-        let imgObj = post.multimedia.find(media => media.format === "superJumbo");
-        post.image_url =imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
-      });
-
-      //Put Array into Chunks
-      let i, j, chunkedArray = [], chunk = 4;
-      for (i=0, j=0; i < posts.length; i += chunk, j++){
-        chunkedArray[j] = posts.slice(i, i+chunk);
-      }
-      return chunkedArray;
-    }
   }
+
 });
